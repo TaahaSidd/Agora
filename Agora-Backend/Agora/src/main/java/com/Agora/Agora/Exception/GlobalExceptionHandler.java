5974936ebe1421e702a60b05d0b1db56e78016d7
Sidd <tahasidd33@gmail.com>
2026-01-17 18:53:35 +0300

    Create globalExceptHandler

---
+++ Agora-Backend/Agora/src/main/java/com/Agora/Agora/Exception/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.Agora.Agora.Exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String,String>> handleAllErrors(Exception ex){
+        Map<String,String> error = new HashMap<>();
+
+        error.put("message", "Something went wrong.Please try again later");
+        error.put("type", "SERVER_ERROR");
+
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
