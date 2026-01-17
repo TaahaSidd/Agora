177f4648e1ce03507f186a67ca87e68b5772623a
Sidd <tahasidd33@gmail.com>
2026-01-17 18:50:03 +0300

    update app.java

--- Agora-Backend/Agora/src/main/java/com/Agora/Agora/AgoraBackendApplication.java
+++ Agora-Backend/Agora/src/main/java/com/Agora/Agora/AgoraBackendApplication.java
@@ -3,11 +3,13 @@ package com.Agora.Agora;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableAsync
 public class AgoraBackendApplication {
 
 	public static void main(String[] args) {
