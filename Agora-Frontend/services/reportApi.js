cbbd781187e729a0c06634416472f3f4928456ba
Sidd <tahasidd33@gmail.com>
2026-01-19 20:52:55 +0300

    Update reportApi.js

--- Agora-Frontend/services/reportApi.js
+++ Agora-Frontend/services/reportApi.js
@@ -1,15 +1,5 @@
 import { apiPost } from "./api";
 
-/**
- * Report something (listing, user, chat, etc.)
- * @param {Object} reportData
- * @param {'LISTING'|'USER'|'CHAT_ROOM'|'MESSAGE'} reportData.reportType
- * @param {string} reportData.reportReason
- * @param {number} [reportData.reportedUserId]
- * @param {number} [reportData.reportedListingId]
- * @param {number} [reportData.targetId]
- */
-
 export const makeReport = async (reportData) => {
     try {
         const res = await apiPost("/report/Make", reportData);
