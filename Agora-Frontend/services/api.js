e8a1b0596e84970281085fa899c8a448b96dadd5
Sidd <tahasidd33@gmail.com>
2026-01-19 16:38:10 +0300

    update services

--- Agora-Frontend/services/api.js
+++ Agora-Frontend/services/api.js
@@ -2,10 +2,9 @@ import axios from "axios";
 import * as SecureStore from 'expo-secure-store';
 import {jwtDecode} from 'jwt-decode';
 import {Alert} from 'react-native';
-import {useUserStore} from "../stores/userStore";
 
-const BASE_URL = "http://192.168.8.15:9000/Agora";
-// const BASE_URL = "https://francisca-overjocular-cheryle.ngrok-free.dev";
+//const BASE_URL = "http://192.168.8.15:9000/Agora";
+const BASE_URL = "https://francisca-overjocular-cheryle.ngrok-free.dev/Agora";
 
 const authApi = axios.create({
     baseURL: BASE_URL,
@@ -39,8 +38,8 @@ async function refreshJwt() {
     if (!refreshToken) throw new Error('No refresh token');
 
     try {
-        const res = await authApi.post('/auth/refresh', { refreshToken });
-        const { jwt: newJwt, refreshToken: newRefresh } = res.data;
+        const res = await authApi.post('/auth/refresh', {refreshToken});
+        const {jwt: newJwt, refreshToken: newRefresh} = res.data;
 
         await SecureStore.setItemAsync('accessToken', newJwt);
         if (newRefresh) await SecureStore.setItemAsync('refreshToken', newRefresh);
@@ -66,64 +65,6 @@ async function isJwtExpired(token) {
     const {exp} = jwtDecode(token);
     return Date.now() >= exp * 1000;
 }
-
-// async function refreshJwt() {
-//     const refreshToken = await SecureStore.getItemAsync('refreshToken');
-//     if (!refreshToken) throw new Error('No refresh token');
-//
-//     try {
-//         const res = await api.post('/auth/refresh', {refreshToken});
-//         const {jwt: newJwt, refreshToken: newRefresh} = res.data;
-//
-//         await SecureStore.setItemAsync('accessToken', newJwt);
-//         if (newRefresh) await SecureStore.setItemAsync('refreshToken', newRefresh);
-//         return newJwt;
-//     } catch (err) {
-//         await SecureStore.deleteItemAsync('accessToken');
-//         await SecureStore.deleteItemAsync('refreshToken');
-//         Alert.alert("Session expired", "Please login again");
-//         throw err;
-//     }
-// }
-
-//
-// api.interceptors.request.use(
-//     async (config) => {
-//
-//         const publicEndpoints = [
-//             "/college/colleges",
-//             "/auth/login/otp",
-//             "/auth/signup/otp",
-//             "/auth/check",
-//             "/auth/refresh",
-//             "/auth/forgot-password",
-//             "/auth/reset-password",
-//             "/listing/all",
-//             "/follow",
-//             "/profile/seller/",
-//             "/Review/seller/",
-//             "/listing/popular-categories",
-//         ];
-//
-//         if (!publicEndpoints.some((url) => config.url.includes(url))) {
-//             let token = await SecureStore.getItemAsync("accessToken");
-//
-//             if (await isJwtExpired(token)) {
-//                 token = await refreshJwt();
-//             }
-//
-//             if (token) {
-//                 config.headers.Authorization = `Bearer ${token}`;
-//             }
-//
-//             if (config.data instanceof FormData) {
-//                 config.headers['Content-Type'] = 'multipart/form-data';
-//             }
-//         }
-//         return config;
-//     },
-//     (error) => Promise.reject(error)
-// );
 
 api.interceptors.request.use(
     async (config) => {
@@ -163,32 +104,30 @@ api.interceptors.request.use(
     (error) => Promise.reject(error)
 );
 
-api.interceptors.response.use(
-    (response) => response,
-    async (error) => {
-        const status = error.response?.status;
-        const serverMessage = error.response?.data?.message;
-
-        const token = await SecureStore.getItemAsync('accessToken');
-        const isActuallyGuest = !token;
-
-        if (status === 403) {
-            if (isActuallyGuest) {
-                console.log("🔒 Silent 403: Guest hit protected route.");
-            } else {
-                Alert.alert("Access Denied", "You don't have permission to do this.");
-            }
-        }
-        else if (status === 500) {
-            Alert.alert("Server Error", "Something went wrong on our end.");
-        }
-        else if (serverMessage && status !== 401) {
-            Alert.alert("Agora", serverMessage);
-        }
-
-        return Promise.reject(error);
-    }
-);
+// api.interceptors.response.use(
+//     (response) => response,
+//     async (error) => {
+//         const status = error.response?.status;
+//         const serverMessage = error.response?.data?.message;
+//
+//         const token = await SecureStore.getItemAsync('accessToken');
+//         const isActuallyGuest = !token;
+//
+//         if (status === 403) {
+//             if (isActuallyGuest) {
+//                 console.log("🔒 Silent 403: Guest hit protected route.");
+//             } else {
+//                 Alert.alert("Access Denied", "You don't have permission to do this.");
+//             }
+//         } else if (status === 500) {
+//             Alert.alert("Server Error", "Something went wrong on our end.");
+//         } else if (serverMessage && status !== 401) {
+//             Alert.alert("Agora", serverMessage);
+//         }
+//
+//         return Promise.reject(error);
+//     }
+// );
 
 export const getColleges = async () => {
     const res = await api.get('/college/colleges');
--- Agora-Frontend/services/share.js
+++ Agora-Frontend/services/share.js
@@ -1,16 +1,21 @@
-import { Share, Alert } from 'react-native';
+import {Alert, Share} from 'react-native';
 
-export const shareItem = async ({ type, title, id }) => {
+export const shareItem = async ({type, title, id}) => {
     try {
+
+        const SITE_URL = 'https://spicalabs.netlify.app/products/agora';
         let message = '';
 
         if (type === 'LISTING') {
-            message = `Check out this listing: ${title}\nhttps://studex.com/listing/${id}`;
+            message = `🔥 Check out this deal on Agora: "${title}"\n\nView it here: ${SITE_URL}?listingId=${id}`;
         } else if (type === 'USER') {
-            message = `Check out this user: ${title}\nhttps://studex.com/user/${id}`;
+            message = `Check out ${title}'s profile on Agora!\n\nConnect here: ${SITE_URL}?userId=${id}`;
         }
 
-        await Share.share({ message });
+        await Share.share({
+            message: message,
+            title: 'Agora Campus Marketplace',
+        });
     } catch (error) {
         Alert.alert('Error', 'Failed to share. Try again.');
         console.error(error);
